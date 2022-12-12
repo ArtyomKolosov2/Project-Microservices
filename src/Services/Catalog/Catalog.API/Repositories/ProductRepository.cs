d639e3fb73bb11346058103f12ca846acaaa2f24
ArtyomKolosov2 <artyomkolosov2@users.noreply.github.com>
2022-12-12 18:48:40 +0100

    Catalog api service, docker files

---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace Catalog.API.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly ICatalogContext _catalogContext;
+
+    public ProductRepository(ICatalogContext catalogContext)
+    {
+        _catalogContext = catalogContext ?? throw new ArgumentNullException(nameof(catalogContext));
+    }
+    
+    public async Task<IEnumerable<Product>> GetProducts() => await _catalogContext.Products.AsQueryable().ToListAsync();
+
+    public Task<Product?> GetProduct(ObjectId id) => _catalogContext.Products.AsQueryable().SingleOrDefaultAsync(x => x.Id == id);
+
+    public async Task<IEnumerable<Product>> GetProductByName(string name) =>
+        await _catalogContext.Products.AsQueryable()
+            .Where(x => x.Name == name)
+            .ToListAsync();
+
+    public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)=>
+        await _catalogContext.Products.AsQueryable()
+            .Where(x => x.Category == categoryName)
+            .ToListAsync();
+
+    public Task CreateProduct(Product product)
+    {
+        return _catalogContext.Products.InsertOneAsync(product);
+    }
+
+    public async Task<bool> UpdateProduct(Product product)
+    {
+        var replaceResult = await _catalogContext.Products.ReplaceOneAsync(x => x.Id == product.Id, product);
+        return replaceResult.IsModifiedCountAvailable;
+    }
+
+    public async Task<bool> DeleteProduct(ObjectId id)
+    {
+        var deleteResult = await _catalogContext.Products.DeleteOneAsync(x => x.Id == id);
+        return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+    }
+}
