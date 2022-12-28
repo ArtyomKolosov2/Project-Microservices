fb8625f12679db0b29d993713540efe83ca68655
ArtyomKolosov2 <artyomkolosov2@users.noreply.github.com>
2022-12-28 15:40:18 +0100

    Ordering layers implementation

---
+++ src/Services/Ordering/Ordering.Application/DependencyInjection/RegisterApplicationServices.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+
+namespace Ordering.Application.DependencyInjection;
+
+public static class RegisterApplicationServices
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+        return services;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/Repositories/OrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence.Context;
+
+namespace Ordering.Infrastructure.Persistence.Repositories;
+
+public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+{
+    public OrderRepository(OrderContext dbContext) : base(dbContext)
+    {
+    }
+    
+    public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName) =>
+        await DbContext.Orders
+            .Where(o => o.UserName == userName)
+            .ToListAsync();
+}
